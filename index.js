da298fec16e5609883eb054ce4e807ad8fb314ca
kajalkajuyadav <138011646+kajalkajuyadav@users.noreply.github.com>
2023-07-05 09:51:11 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("kajal")
+const fs = require("fs")
+const http = require("http")
+
+http.createServer((req ,res)=>{
+const filename ="index.html"
+
+    if(req.url=="/home"){
+        fs.readFile(filename,"utf-8",(err,data)=>{
+            if(err){
+                console.log(err)
+                res.writeHead(500,{"content-type":"text/html"})
+                res.end("sever on")
+                return;
+            }
+        })
+    }
+    else if(req.url=="/submit"){
+        fs.readFile(filename,"utf-8",(err,data)=>{
+            // res.send(req.query)
+            res.end(data)
+        })
+     }
+
+
+}).listen(7000)
